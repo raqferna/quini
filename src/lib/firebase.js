97d30735f2fb75a5cd5a5f7506ff8891e25b0114
raquelferigle <raquelferigle@gmail.com>
2026-02-22 11:35:25 +0000

    VERSION BUENA

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,19 @@
-import admin from 'firebase-admin';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Get the credentials from the environment variable
-const serviceAccount = JSON.parse(import.meta.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+const firebaseConfig = {
+  apiKey: "AIzaSyAcldQYjIqVD3BPwRyzocfDTE3Cktxos4s",
+  authDomain: "quini-cms.firebaseapp.com",
+  projectId: "quini-cms",
+  storageBucket: "quini-cms.firebasestorage.app",
+  messagingSenderId: "465095637451",
+  appId: "1:465095637451:web:f12b9f8b1a93a5ca04de4e",
+  measurementId: "G-JFW8CXXZ0H"
+};
 
-// Initialize the app if it hasn't been already
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
-
-// Get the firestore instance
-const db = admin.firestore();
+// Initialize Firebase
+// This pattern prevents re-initializing the app on the server.
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
 
 export { db };
